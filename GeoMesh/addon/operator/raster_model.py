52490e3170dc55baf86b3cb51540234f9c6b3da1
DGRod <DGRodriguezNY@gmail.com>
2024-09-15 19:46:04 -0400

    Added filter to file browser; started work on mesh

--- GeoMesh/addon/operator/raster_model.py
+++ GeoMesh/addon/operator/raster_model.py
@@ -6,38 +6,63 @@ if venv_path not in sys.path:
 
 # Import Blender modules
 import bpy
+import bmesh
 from bpy_extras.io_utils import ImportHelper
 
-# Import GDAL and Numpy
+# Import GDAL and math tools
 from osgeo import gdal
 import numpy as np
+import pandas as pd
+import csv
 
 
-print(gdal.__version__)
-print(sys.executable)
+
+
+
 class GEOMESH_OT_RASTER_MODEL(bpy.types.Operator, ImportHelper):
     """Import and model a Raster file"""
-    bl_label = "Model Raster"
+    bl_label = "Model Raster (TIFF)"
     bl_idname = "geomesh.raster_model"
 
     # Store filepath to Raster here
     fn: bpy.props.StringProperty(name="")
+    # Filter file browser for TIFF files
+    filter_glob: bpy.props.StringProperty(
+        default="*.tif;*.xyz",
+        options={"HIDDEN"})
+    
 
     def execute(self, context):
         # Ask the user to select a Raster file
         self.fn = self.filepath
         
-        ds = gdal.Open(self.fn)
-        gt = ds.GetGeoTransform()
-        proj = ds.GetProjection()
+        # Open Raster with GDAL
+        # ds = gdal.Open(self.fn)
+        # gt = ds.GetGeoTransform()
+        # proj = ds.GetProjection()
+        
+        # If filetype is .xyz
+        if self.fn[-4:] == ".xyz":
+            # df = pd.read_csv(self.fn, sep=" ", header=None)
+            # df.to_csv("C:\\Users\\DGRod\\OneDrive\\Desktop\\Python Code\\Blender GeoMesh\\GeoMesh\\addon\\operator\\data\\mesh_data.csv", index=False, header=None, sep=",")
+            ds = gdal.Open(self.fn)
+            band = ds.GetRasterBand(1)
+            array = band.ReadAsArray()
 
-        band = ds.GetRasterBand(1)
-        array = band.ReadAsArray()
-
-        print(gt, proj, array)
-
-        for point in array:
-            print("point", point)
-
+            mesh_obj = bpy.ops.mesh.primitive_plane_add(size=100)
+            #bpy.context.collection.link(mesh_obj)
+            # mesh_obj.select_set(state=True)
+            # bpy.context.view_layer.objects.active = mesh_obj
+            bm = bmesh.new()
+            bm.from_mesh(bpy.context.object.data)
+            
+            for row in array:
+                print("row", row)
+                bm.verts.new(coord)
+            
+            bm.to_mesh(mesh_obj)
+            bm.free()
+        
+        
         return {"FINISHED"}
     
