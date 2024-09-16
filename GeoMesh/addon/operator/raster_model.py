f30e1694cae498645e0d3f62fa5d313410fd44ea
DGRod <DGRodriguezNY@gmail.com>
2024-09-16 13:05:43 -0400

    raster_model is operational for .xyz files

--- GeoMesh/addon/operator/raster_model.py
+++ GeoMesh/addon/operator/raster_model.py
@@ -13,7 +13,7 @@ from bpy_extras.io_utils import ImportHelper
 from osgeo import gdal
 import numpy as np
 import pandas as pd
-import csv
+import math
 
 
 
@@ -43,25 +43,44 @@ class GEOMESH_OT_RASTER_MODEL(bpy.types.Operator, ImportHelper):
         
         # If filetype is .xyz
         if self.fn[-4:] == ".xyz":
-            # df = pd.read_csv(self.fn, sep=" ", header=None)
+            df = pd.read_csv(self.fn, sep=" ", header=None)
+            df.columns = ["x", "y", "z"]
             # df.to_csv("C:\\Users\\DGRod\\OneDrive\\Desktop\\Python Code\\Blender GeoMesh\\GeoMesh\\addon\\operator\\data\\mesh_data.csv", index=False, header=None, sep=",")
-            ds = gdal.Open(self.fn)
-            band = ds.GetRasterBand(1)
-            array = band.ReadAsArray()
+            # ds = gdal.Open(self.fn)
+            # band = ds.GetRasterBand(1)
+            # array = band.ReadAsArray()
+            res = int(math.sqrt(len(df)))
 
-            mesh_obj = bpy.ops.mesh.primitive_plane_add(size=100)
+            mins = df.min()
+            xmin = mins["x"]
+            ymin = mins["y"]
+            zmin = mins["z"]
+            print(mins)
+
+            coords = [((x - xmin)/100, (y - ymin)/100, (z - zmin)/100) for x, y, z in zip(df["x"], df["y"], df["z"])]
+            
+            bpy.ops.mesh.primitive_grid_add(x_subdivisions=res - 1, y_subdivisions=res - 1) 
+            mesh_data = bpy.context.object.data
+            
+            bpy.ops.object.mode_set(mode="OBJECT")
+
+            
             #bpy.context.collection.link(mesh_obj)
             # mesh_obj.select_set(state=True)
             # bpy.context.view_layer.objects.active = mesh_obj
             bm = bmesh.new()
-            bm.from_mesh(bpy.context.object.data)
+            bm.from_mesh(mesh_data)
+
+            print(len(bm.verts), len(coords))
             
-            for row in array:
-                print("row", row)
-                bm.verts.new(coord)
+            for vertex, coord in zip(bm.verts, coords):
+                vertex.co = coord
             
-            bm.to_mesh(mesh_obj)
+
+            bm.to_mesh(mesh_data)
             bm.free()
+
+            print(mesh_data.vertices)
         
         
         return {"FINISHED"}
